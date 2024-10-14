15e68ca58ee0bc6e40a50ebd4b920ade37108008
Ras Ismael Barbosa <164416743+rasismelbarbosa@users.noreply.github.com>
2024-10-14 13:28:15 -0300

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,12 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
- apiKey: "AIzaSyDmnjxFeRfIzIQNaCTUxclVY35BDooVjYQ",
-  authDomain: "projetosrasismael.firebaseapp.com",
-  projectId: "projetosrasismael",
-  storageBucket: "projetosrasismael.appspot.com",
-  messagingSenderId: "315979474282",
-  appId: "1:315979474282:web:e92a3af08588fa496f7113",
+ apiKey: 'api-key',
+  authDomain: 'project-id.firebaseapp.com',
+  databaseURL: 'https://project-id.firebaseio.com',
+  projectId: 'project-id',
+  storageBucket: 'project-id.appspot.com',
+  messagingSenderId: 'sender-id',
+  appId: 'app-id',
+  measurementId: 'G-measurement-id',
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
